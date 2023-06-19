8168eb4b9ab081e84e3d4ca1b1b79f0c6f64e085
Arasch Lakghomi <arasch@lakghomi.com>
2023-06-19 10:02:22 +0200

    feat: install ws as dependecy and implement sample ws server as preparation for ui-client connection

--- ws-server/index.js
+++ ws-server/index.js
@@ -1,1 +1,21 @@
-console.log('This will be the server')
+const WebSocketServer = require('ws');
+const wss = new WebSocketServer.Server({ port: 3000 })
+
+wss.on("connection", ws => {
+    console.log("new client connected");
+    ws.send('Welcome, you are connected!');
+
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+    });
+
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
+
+console.log("The WebSocket server is running on port 3000");
